d962e49984ace7d1be7f162978af460630bcd2c6
Naveen <naveen@digispheretech.in>
2026-06-12 14:49:49 +0530

    Develop contactus api

---
+++ Controller/contactusController.js
@@ -0,0 +1,59 @@
+const pool = require('../db/db');
+const transporter = require("../utils/mailConfig");
+const { sendContactMail } = require("../utils/mailConfig");
+
+exports.contactUs = async (req, res) => {
+  const {
+    name,
+    phone_number,
+    email,
+    message,
+  } = req.body;
+  
+  if (
+    !name ||
+    !phone_number ||
+    !email ||
+    !message
+  ) {
+    return res.status(400).json({
+      statusCode: 400,
+      message: "Missing required fields",
+    });
+  }
+
+  try {
+    // Save to DB
+    const result = await pool.query(
+      `INSERT INTO tbl_contact_us
+            (name, phone_number, email, message)
+            VALUES ($1,$2,$3,$4)
+            RETURNING *`,
+      [
+        name,
+        phone_number,
+        email,
+        message,
+      ],
+    );
+
+    // Send Mail
+    await sendContactMail({
+      name,
+      phone_number,
+      email,
+      message,
+    });
+
+    return res.status(200).json({
+      statusCode: 200,
+      message: "Contact submitted & mail sent successfully",
+      data: result.rows[0],
+    });
+  } catch (error) {
+    return res.status(500).json({
+      statusCode: 500,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
---
+++ routes/contactusroutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { contactUs } = require("../Controller/contactusController");
+
+router.post("/", contactUs);
+
+module.exports = router;
