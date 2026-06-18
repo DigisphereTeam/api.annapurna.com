f994b6a3d4d6fdb4f93998732f20cd5ae09e1008
Naveen <naveen@digispheretech.in>
2026-06-18 17:38:23 +0530

    fix upload module in carousel routes

--- routes/carouselroutes.js
+++ routes/carouselroutes.js
@@ -1,19 +1,17 @@
-const carouselcontroller=require('../Controller/carouselController');
-const  express=require('express');
-const router= express.Router();
-const upload=require('../utils/uploadfile');
-const uploadfle=require('../utils/fileupload');
+const carouselcontroller = require('../Controller/carouselController');
+const express = require('express');
+const router = express.Router();
+const upload = require('../utils/fileupload');
 
 router.post('/addcarousel', upload.array('carousel_image', 5), carouselcontroller.addCarousel);
 
 
-router.get('/getallcarousel',carouselcontroller.getallCarousels);
-router.post('/getcarouselByid',carouselcontroller.getcarouselByid);
-router.post('/updatecarousel',uploadfle.fields([
-    {name:'carousel_image',maxCount:1}]),carouselcontroller.updatecarousel);
+router.get('/getallcarousel', carouselcontroller.getallCarousels);
+router.post('/getcarouselByid', carouselcontroller.getcarouselByid);
+router.post('/updatecarousel', upload.fields([
+    { name: 'carousel_image', maxCount: 1 }]), carouselcontroller.updatecarousel);
 
-router.post('/deletecarousel',carouselcontroller.deleteCarousel);
+router.post('/deletecarousel', carouselcontroller.deleteCarousel);
 
 
-module.exports=router
- 
+module.exports = router
